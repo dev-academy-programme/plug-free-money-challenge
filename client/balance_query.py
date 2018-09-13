3748cdf12778e92dd1c8830964b24c738a498e1a
Joshua Vial <joshua@enspiral.com>
2018-09-13 18:45:13 +1200

    removing unused libraries

--- client/balance_query.py
+++ client/balance_query.py
@@ -1,14 +1,5 @@
 from plug.key import ED25519SigningKey
-from plug.hash import sha256
-from plug.proof import SingleKeyProof
-from balance_tutorial.transform import BalanceQuery
-from plug.transaction import Transaction
-from plug.constant import TransactionEvent
-from plug.message import Event
-from plug.registry import Registry
 import aiohttp
-import json
-import asyncio
 from balance_tutorial.user import User
 
 async def init_balance_query(signing_key_input):
