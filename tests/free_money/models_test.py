048f996950ac55205a496666cbc18ffc26e28f9d
harrison-symes <symeshjb@gmail.com>
2018-09-17 17:07:50 +1200

    basic class tests for models

---
+++ tests/free_money/models_test.py
@@ -0,0 +1,24 @@
+import pytest
+
+from free_money.model import BalanceModel
+
+def test_balance_model_initial_balance():
+    """ARRANGE"""
+    expected_balance = 0
+
+    """ACT"""
+    model = BalanceModel()
+
+    """ASSERT"""
+    assert model.balance == expected_balance
+
+def test_balance_model_set_balance():
+    """ARRANGE"""
+    balance = 100
+    expected_balance = balance
+
+    """ACT"""
+    model = BalanceModel(balance)
+
+    """ASSERT"""
+    assert model.balance == expected_balance
