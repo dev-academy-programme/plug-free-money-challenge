15a514542f07e9506d6592dec60574fec3a660f7
harrison-symes <symeshjb@gmail.com>
2018-09-18 15:52:41 +1200

    trim imports and refactor

--- client/free_money_client.py
+++ client/free_money_client.py
@@ -1,33 +1,23 @@
-from plug.key import ED25519SigningKey
-from plug.hash import sha256
-from plug.proof import SingleKeyProof
-from free_money.transform import FreeMoney
-from plug.transaction import Transaction
-from plug.constant import TransactionEvent
 from plug.message import Event
 from plug.registry import Registry
-from user import User
-import aiohttp
-import json
-import asyncio
 
 from plug_api.clients.v1 import PlugApiClient
 from plug_api.key_managers.sqlite import SqliteKeyManager
 
+from free_money.transform import FreeMoney
+from user import User
 
 async def init_free_money(address_input):
     registry = Registry().with_default()
     registry.register(Event)
     registry.register(FreeMoney)
 
-    user = await User.load(address_input)
     key_manager = SqliteKeyManager('keys.db').setup()
     client = PlugApiClient("http://localhost:8181", key_manager)
 
-    transform = FreeMoney(
+    response = client.broadcast_transform(FreeMoney(
         receiver=address_input,
         amount=1000,
-    )
+    ))
 
-    response = client.broadcast_transform(transform)
     print(response)
--- client/user.py
+++ client/user.py
@@ -1,19 +1,12 @@
-from plug.key import ED25519SigningKey
-from plug.util import plug_address
-
-import aiohttp
-import json
-import asyncio
-
 from plug_api.clients.v1 import PlugApiClient
 from plug_api.key_managers.sqlite import SqliteKeyManager
 
 class User:
-    def __init__(self, address):
-        client = PlugApiClient("http://localhost:8181", "keys.db")
-        network_id = client.network_id
-        key_manager = SqliteKeyManager('keys.db').setup()
+    client = PlugApiClient("http://localhost:8181", "keys.db")
+    network_id = client.network_id
+    key_manager = SqliteKeyManager('keys.db').setup()
 
+    def __init__(self, address):
         if (address):
             self.address = address
         else:
@@ -27,7 +20,4 @@ class User:
         return user
 
     async def get_nonce(self):
-        client = PlugApiClient("http://localhost:8181", "keys.db")
-        network_id = client.network_id
-        key_manager = SqliteKeyManager('keys.db').setup()
-        return key_manager.get_nonce(self.address, network_id)
+        return self.key_manager.get_nonce(self.address, self.network_id)
