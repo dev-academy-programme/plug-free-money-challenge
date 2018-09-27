83a401ce2331062bcf1eefcda5bb6004fdbd0768
josephquested <josephquested@gmail.com>
2018-09-27 13:54:14 +1200

    fix balance transfer client side

--- client/commands/transaction.py
+++ client/commands/transaction.py
@@ -1,4 +1,5 @@
 from free_money.transform import BalanceTransfer
+from asyncio import get_event_loop
 
 from client.utils import register_transform_event
 from client.user import User
@@ -6,10 +7,12 @@ from client.user import User
 def init_transaction(client, sender_key_input, receiver_address, amount):
     register_transform_event(BalanceTransfer)
 
-    response = client.broadcast_transform(BalanceTransfer(
+    loop = get_event_loop()
+
+    response = loop.run_until_complete(client.broadcast_transform(BalanceTransfer(
         sender=sender_key_input,
         receiver=receiver_address,
         amount=int(amount)
-    ))
+    )))
 
     print(response)
