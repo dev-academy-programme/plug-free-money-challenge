c47a1496878768b7234f4f69bc59ad9f462ed148
josephquested <josephquested@gmail.com>
2018-09-28 11:10:26 +1200

    add free money and transform solution code

--- client/commands/free_money_client.py
+++ client/commands/free_money_client.py
@@ -1,16 +1,18 @@
-from plug.key import ED25519SigningKey
-from plug.hash import sha256
-from plug.proof import SingleKeyProof
 from free_money.transform import FreeMoney
-from plug.transaction import Transaction
-from plug.constant import TransactionEvent
-from plug.message import Event
-from plug.registry import Registry
+from client.utils import register_transform_event
 from client.user import User
-import aiohttp
-import json
-import asyncio
 
-async def init_free_money(client, input_key, amount):
+from asyncio import get_event_loop
 
-    print("Free Money!")
+async def init_free_money(client, address_input, amount):
+    register_transform_event(FreeMoney)
+
+    loop = get_event_loop()
+
+    response = loop.run_until_complete(client.broadcast_transform(FreeMoney(
+        receiver=address_input,
+        amount=int(amount),
+    )))
+
+    print(response)
+    return response
--- free_money/transform.py
+++ free_money/transform.py
@@ -14,29 +14,81 @@ class BalanceTransfer(Transform):
     amount: int
 
     def required_authorizations(self):
-        print("place_holder!")
+        return {self.sender}
 
     @staticmethod
     def required_models():
-        print("place_holder!")
+        return {BalanceModel.fqdn}
 
     def required_keys(self):
-        print("place_holder!")
+        return {self.sender, self.receiver}
 
     @staticmethod
     def pack(registry, obj):
-        print("place_holder!")
+        return {
+            "sender": obj.sender,
+            "receiver": obj.receiver,
+            "amount": obj.amount,
+        }
 
     @classmethod
     def unpack(cls, registry, payload):
-        print("place_holder!")
+        return cls(
+            sender=payload["sender"],
+            receiver=payload["receiver"],
+            amount=payload["amount"],
+        )
 
     def verify(self, state_slice):
-        print("place_holder!")
+        balances = state_slice[BalanceModel.fqdn]
+
+        if self.amount <= 0:
+            raise free_money.error.InvalidAmountError("Transfer amount must be more than 0")
+
+        if balances[self.sender].balance < self.amount:
+            raise free_money.error.NotEnoughMoneyError("Insufficient funds")
 
     def apply(self, state_slice):
-        print("place_holder!")
+        balances = state_slice[BalanceModel.fqdn]
+        balances[self.sender].balance -= self.amount
+        balances[self.receiver].balance += self.amount
 
 @dataclass
 class FreeMoney(Transform):
-    print("Welcome to the Free Money challenge!")
+    fqdn = "tutorial.FreeMoney"
+    receiver: str
+    amount: int
+
+    def required_authorizations(self):
+        return {self.receiver}
+
+    @staticmethod
+    def required_models():
+        return {BalanceModel.fqdn}
+
+    def required_keys(self):
+        return {self.receiver}
+
+    @staticmethod
+    def pack(registry, obj):
+        return {
+            "receiver": obj.receiver,
+            "amount": obj.amount,
+        }
+
+    @classmethod
+    def unpack(cls, registry, payload):
+        return cls(
+            receiver=payload["receiver"],
+            amount=payload["amount"],
+        )
+
+    def verify(self, state_slice):
+        balances = state_slice[BalanceModel.fqdn]
+
+        if self.amount <= 0:
+            raise free_money.error.InvalidAmountError("Transfer amount must be more than 0")
+
+    def apply(self, state_slice):
+        balances = state_slice[BalanceModel.fqdn]
+        balances[self.receiver].balance += self.amount
